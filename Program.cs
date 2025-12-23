18465d2b143864fdc640e68177617227166e8e56
Gabriel Veloso <gabrielveloso2@gmail.com>
2025-12-23 15:09:29 +0100

    first commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Nokia nokia  = new Nokia("123456", "Nokia 10", "kae", 300);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Iphone iphone  = new Iphone("123456", "iphone 12", "kae", 500);
+iphone.Ligar();
+iphone.InstalarAplicativo("Telegram");
